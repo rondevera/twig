133a42ab6a64e2611bccfd7de1b9da19415a1454
Ron DeVera <hello@rondevera.com>
2012-10-04 11:06:05 -0700

    Move config file parsing out of `bin/twig`

--- lib/twig/options.rb
+++ lib/twig/options.rb
@@ -1,5 +1,28 @@
 class Twig
   module Options
+    CONFIG_FILE = '~/.twigrc'
+
+    def read_config_file
+      config_file_path = File.expand_path(Twig::CONFIG_FILE)
+      return unless File.readable?(config_file_path)
+
+      File.open(config_file_path) do |f|
+        opts = f.read.split("\n").inject({}) do |hsh, opt|
+          key, value = opt.split(':', 2)
+          hsh.merge(key.strip => value.strip)
+        end
+
+        opts.each do |key, value|
+          case key
+          when 'b', 'branch'  then set_option(:branch,       value)
+          when 'max-days-old' then set_option(:max_days_old, value)
+          when 'only-name'    then set_option(:name_only,    value)
+          when 'except-name'  then set_option(:name_except,  value)
+          end
+        end
+      end
+    end
+
     def set_option(key, value)
       case key
       when :branch
