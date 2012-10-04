44300ac0b4f9c7b236282a451e5b04012ad0ec55
Ron DeVera <hello@rondevera.com>
2012-10-03 17:43:31 -0700

    Remove unused `#hash_slice` method

--- lib/twig/util.rb
+++ lib/twig/util.rb
@@ -5,12 +5,5 @@ class Twig
       !!Float(value) rescue false
     end
 
-    def self.hash_slice(hsh, wanted_keys)
-      hsh.inject({}) do |h, (k, v)|
-        h[k] = v if wanted_keys.include?(k)
-        h
-      end
-    end
-
   end
 end
