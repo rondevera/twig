34ecf06ef11406fef19091e903f49e256372f303
Ron DeVera <hello@rondevera.com>
2013-05-19 05:50:55 -0700

    Add `Twig::Util.truthy?`

--- lib/twig/util.rb
+++ lib/twig/util.rb
@@ -5,5 +5,9 @@ class Twig
       !!Float(value) rescue false
     end
 
+    def self.truthy?(value)
+      %w[true yes y on 1].include?(value.to_s.downcase)
+    end
+
   end
 end
--- spec/twig/util_spec.rb
+++ spec/twig/util_spec.rb
@@ -13,4 +13,28 @@ describe Twig::Util do
       Twig::Util.numeric?({}).should be_false
     end
   end
+
+  describe '.truthy?' do
+    it 'returns true if an object is truthy' do
+      Twig::Util.truthy?('true').should be_true
+      Twig::Util.truthy?('TRUE').should be_true
+      Twig::Util.truthy?(true).should be_true
+      Twig::Util.truthy?('yes').should be_true
+      Twig::Util.truthy?('YES').should be_true
+      Twig::Util.truthy?('y').should be_true
+      Twig::Util.truthy?('Y').should be_true
+      Twig::Util.truthy?('on').should be_true
+      Twig::Util.truthy?('ON').should be_true
+      Twig::Util.truthy?('1').should be_true
+      Twig::Util.truthy?(1).should be_true
+    end
+
+    it 'returns false if an object is falsy' do
+      Twig::Util.truthy?('false').should be_false
+      Twig::Util.truthy?(false).should be_false
+      Twig::Util.truthy?('yep').should be_false
+      Twig::Util.truthy?('sure, why not').should be_false
+    end
+  end
+
 end
