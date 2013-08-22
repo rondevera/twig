6290b8b4cdecec934c5cd52b89fe0148547d3aee
Ron DeVera <hello@rondevera.com>
2013-08-22 07:25:32 -0700

    Add `Twig::Subcommands::BIN_PREFIX`

--- lib/twig/subcommands.rb
+++ lib/twig/subcommands.rb
@@ -1,18 +1,20 @@
 class Twig
   module Subcommands
 
+    BIN_PREFIX = 'twig-'
+
     def self.all_names
       bin_paths = []
 
       bin_dir_paths.each do |bin_dir_path|
-        path_pattern = File.join(bin_dir_path, 'twig-*')
+        path_pattern = File.join(bin_dir_path, BIN_PREFIX + '*')
         Dir.glob(path_pattern) do |bin_path|
           bin_paths << File.basename(bin_path)
         end
       end
 
       bin_paths.uniq.sort.map do |bin_path|
-        bin_path.sub(/^twig-/, '')
+        bin_path.sub(Regexp.new('^' << BIN_PREFIX), '')
       end
     end
 
