a4d021eb1e428ea2326fdbf09ebb08ed2d819807
Ron DeVera <hello@rondevera.com>
2013-09-09 08:23:03 -0700

    Switch to `expect` syntax in util_spec.rb

--- spec/twig/util_spec.rb
+++ spec/twig/util_spec.rb
@@ -3,37 +3,37 @@ require 'spec_helper'
 describe Twig::Util do
   describe '.numeric?' do
     it 'returns true if an object is numeric' do
-      Twig::Util.numeric?(1).should be_true
-      Twig::Util.numeric?('1').should be_true
+      expect(Twig::Util.numeric?(1)).to be_true
+      expect(Twig::Util.numeric?('1')).to be_true
     end
 
     it 'returns false if an object is not numeric' do
-      Twig::Util.numeric?('x').should be_false
-      Twig::Util.numeric?([]).should be_false
-      Twig::Util.numeric?({}).should be_false
+      expect(Twig::Util.numeric?('x')).to be_false
+      expect(Twig::Util.numeric?([])).to be_false
+      expect(Twig::Util.numeric?({})).to be_false
     end
   end
 
   describe '.truthy?' do
     it 'returns true if an object is truthy' do
-      Twig::Util.truthy?('true').should be_true
-      Twig::Util.truthy?('TRUE').should be_true
-      Twig::Util.truthy?(true).should be_true
-      Twig::Util.truthy?('yes').should be_true
-      Twig::Util.truthy?('YES').should be_true
-      Twig::Util.truthy?('y').should be_true
-      Twig::Util.truthy?('Y').should be_true
-      Twig::Util.truthy?('on').should be_true
-      Twig::Util.truthy?('ON').should be_true
-      Twig::Util.truthy?('1').should be_true
-      Twig::Util.truthy?(1).should be_true
+      expect(Twig::Util.truthy?('true')).to be_true
+      expect(Twig::Util.truthy?('TRUE')).to be_true
+      expect(Twig::Util.truthy?(true)).to be_true
+      expect(Twig::Util.truthy?('yes')).to be_true
+      expect(Twig::Util.truthy?('YES')).to be_true
+      expect(Twig::Util.truthy?('y')).to be_true
+      expect(Twig::Util.truthy?('Y')).to be_true
+      expect(Twig::Util.truthy?('on')).to be_true
+      expect(Twig::Util.truthy?('ON')).to be_true
+      expect(Twig::Util.truthy?('1')).to be_true
+      expect(Twig::Util.truthy?(1)).to be_true
     end
 
     it 'returns false if an object is falsy' do
-      Twig::Util.truthy?('false').should be_false
-      Twig::Util.truthy?(false).should be_false
-      Twig::Util.truthy?('yep').should be_false
-      Twig::Util.truthy?('sure, why not').should be_false
+      expect(Twig::Util.truthy?('false')).to be_false
+      expect(Twig::Util.truthy?(false)).to be_false
+      expect(Twig::Util.truthy?('yep')).to be_false
+      expect(Twig::Util.truthy?('sure, why not')).to be_false
     end
   end
 
