5566d59d3cb2cd626c6a9c3ef9f6ee37bd5a3619
Ron DeVera <hello@rondevera.com>
2013-09-17 09:04:18 -0700

    Switch to `allow` syntax in subcommands_spec.rb

--- spec/twig/subcommands_spec.rb
+++ spec/twig/subcommands_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Twig::Subcommands do
   describe '.all_names' do
     it 'returns a unique, sorted list of known subcommands' do
-      Twig::Subcommands.stub(:bin_dir_paths) { %w[foo/bin bar/bin] }
+      allow(Twig::Subcommands).to receive(:bin_dir_paths) { %w[foo/bin bar/bin] }
       expect(Dir).to receive(:glob).with('foo/bin/twig-*').
         and_yield('foo/bin/twig-subcommand-2').
         and_yield('foo/bin/twig-subcommand-1')
@@ -16,7 +16,7 @@ describe Twig::Subcommands do
     end
 
     it 'returns an empty array if no subcommands are found' do
-      Twig::Subcommands.stub(:bin_dir_paths) { %w[foo/bin bar/bin] }
+      allow(Twig::Subcommands).to receive(:bin_dir_paths) { %w[foo/bin bar/bin] }
       expect(Dir).to receive(:glob).with('foo/bin/twig-*')
       expect(Dir).to receive(:glob).with('bar/bin/twig-*')
 
