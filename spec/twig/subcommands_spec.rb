c52037656fc8d6591ef3d29ecb17a95aef26d636
Ron DeVera <hello@rondevera.com>
2014-03-28 09:16:50 -0700

    Improve `Twig::Subcommands` spec coverage

--- spec/twig/subcommands_spec.rb
+++ spec/twig/subcommands_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Twig::Subcommands do
   describe '.all_names' do
     it 'returns a unique, sorted list of known subcommands' do
-      allow(Twig::Subcommands).to receive(:bin_dir_paths) { %w[foo/bin bar/bin] }
+      stub_const('ENV', ENV.to_hash.merge('PATH' => 'foo/bin:bar/bin'))
       expect(Dir).to receive(:glob).with('foo/bin/twig-*').
         and_yield('foo/bin/twig-subcommand-2').
         and_yield('foo/bin/twig-subcommand-1')
@@ -16,7 +16,7 @@ describe Twig::Subcommands do
     end
 
     it 'returns an empty array if no subcommands are found' do
-      allow(Twig::Subcommands).to receive(:bin_dir_paths) { %w[foo/bin bar/bin] }
+      stub_const('ENV', ENV.to_hash.merge('PATH' => 'foo/bin:bar/bin'))
       expect(Dir).to receive(:glob).with('foo/bin/twig-*')
       expect(Dir).to receive(:glob).with('bar/bin/twig-*')
 
